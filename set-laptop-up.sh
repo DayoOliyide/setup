02e49604c4356f935b05c94c6fffdc9f95d749c6
DayoOliyide <dayo.oliyide@gmail.com>
2023-05-12 14:12:02 +0100

    Add pkg install function and installing git

--- set-laptop-up.sh
+++ set-laptop-up.sh
@@ -15,6 +15,18 @@ snap_install () {
 }
 
 
+pkg_install () {
+    printf "Installing ${1} ...."
+    install_status=$(dpkg-query -W --showformat='${db:Status-Status}' ${1} 2>&1)
+    if [ $? -eq 0 ] || [ $status = "installed" ]
+    then
+        printf " already installed. \n"
+    else
+        apt-get install ${1}
+    fi
+}
+
+
 ###### MAIN
 if [ "$EUID" -ne 0 ];then
     echo "Please run this script as root user or sudo it"
@@ -26,3 +38,9 @@ fi
 
 snap_install chromium
 snap_install brave
+
+
+
+###### DEVELOPMENT #######
+
+pkg_install git
