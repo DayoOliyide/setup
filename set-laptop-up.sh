c6ca6858426e6c4dbfe00d8ac58b25430c26b01b
DayoOliyide <dayo.oliyide@gmail.com>
2023-08-27 00:28:22 +0100

    Added flatpak install function and installing KeepassXC

--- set-laptop-up.sh
+++ set-laptop-up.sh
@@ -1,8 +1,54 @@
 #!/bin/bash
 
+check_for_flathub_repo () {
+    printf "Checking that flathub repo has been added ...."
+    flathub_matches=$(flatpak remotes | egrep -c '^flathub\s')    
+    if [ "$flathub_matches" -eq 0 ]
+    then
+        printf " flathub repo NOT found!\n"
+        printf "Adding flathub repo .....\n"
+        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+        if [ $? -ne 0 ]
+        then
+            printf "Adding flathub repo has failed!!! Exiting script!\n"
+            exit 2
+        fi
+        printf "flathub repo added\n"
+        printf "For flathub repo to be picked and used in flatpak later installs, System reboot required now\n" 
+        read -p "REBOOT SYSTEM ? (y/n) " should_reboot
+        if [ "$should_reboot" = "y" ]
+        then
+            shutdown -r now 
+        else
+            printf "################################################################\n"
+            printf "Exiting setup script as reboot needed for any flatpak installs!!\n"
+            printf "Please reboot system before running this script again!!!\n"
+            printf "################################################################\n"
+            exit 3
+        fi
+    else
+        printf " flathub repo found\n"
+    fi
+}
+
+
+
+flatpak_install () {
+    printf "Installing flatpak ${1} ...."
+    flatpak_matches=$(flatpak list --app | egrep -c "\s${1}\s")
+    if [ "$flatpak_matches" -eq 0 ]
+    then
+        flatpak install -y flathub ${1}
+        printf " done\n"
+    else
+        printf " already installed. \n"
+    fi
+}
+
+
 
 snap_install () {
-    printf "Installing ${1} ...."
+    printf "Installing snap ${1} ...."
     snap_info_output=$(snap list ${1})
     if [ $? -eq 0 ]
     #TODO if a verbose parameter is given, output snap_info_output
@@ -44,15 +90,19 @@ if [ "$EUID" -ne 0 ];then
 fi
 
 
-###### TOOLS ######
+###### CORE ######
 pkg_install openssh-server
+
+pkg_install flatpak
+pkg_install gnome-software-plugin-flatpak
+check_for_flathub_repo
 
 
 ###### ESSENTIALS #######
 
 snap_install chromium
 snap_install brave
-
+flatpak_install org.keepassxc.KeePassXC
 
 
 ###### DEVELOPMENT #######
